7104314f6dfa60f5c9fc6ec681a3b447050b522d
carsonmorris <carsonmorris548@u.boisestate.edu>
2023-12-02 19:35:55 -0700

    Got buttons linking to new pages

--- App.js
+++ App.js
@@ -1,26 +1,92 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { ScrollView } from 'react-native';
+
+// Workouts overlay component
+const WorkoutsOverlay = ({ onClose }) => {
+  const [newButtonName, setNewButtonName] = useState('');
+  const [showAddButton, setShowAddButton] = useState(false);
+  const [buttons, setButtons] = useState([]); // Initial buttons
+
+  const handleAddButton = () => {
+    setShowAddButton(true);
+  };
+
+  const handleSaveButton = () => {
+    if (newButtonName.trim() !== '') {
+      setButtons([...buttons, newButtonName]);
+      setNewButtonName('');
+      setShowAddButton(false);
+    }
+  };
+
+  return (
+    <View style={styles.overlayContainer}>
+      <Text style={styles.title}>Workouts</Text>
+
+      {buttons.map((button, index) => (
+        <TouchableOpacity key={index} style={styles.button}>
+          <Text style={styles.buttonText}>{button}</Text>
+        </TouchableOpacity>
+      ))}
+
+      {showAddButton ? (
+        <View style={styles.addContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Workout"
+            onChangeText={(text) => setNewButtonName(text)}
+          />
+          <TouchableOpacity style={styles.addButton} onPress={handleSaveButton}>
+            <Text style={styles.buttonText}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <TouchableOpacity style={styles.addButton} onPress={handleAddButton}>
+          <Text style={styles.buttonText}>+ New Workout</Text>
+        </TouchableOpacity>
+      )}
+
+      <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+        <Text style={styles.buttonText}>Close</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+// BodyStats overlay component
+const BodyStatsOverlay = ({ onClose }) => {
+  return (
+    <View style={styles.overlayContainer}>
+      <Text style={styles.title}>Body Stats</Text>
+      {/* Add your Body Stats content here */}
+      <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+        <Text style={styles.buttonText}>Close</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function App() {
   const [showHelp, setShowHelp] = useState(false);
+  const [showWorkouts, setShowWorkouts] = useState(false);
+  const [showBodyStats, setShowBodyStats] = useState(false);
 
   const handleWorkoutsPress = () => {
-    // Navigate to the Workouts screen TODO
-    console.log('Navigate to Workouts screen');
+    setShowWorkouts(true);
   };
 
   const handleBodyStatsPress = () => {
-    // Navigate to the Body Stats screen TODO
-    console.log('Navigate to Body Stats screen');
+    setShowBodyStats(true);
   };
 
   const handleHelpPress = () => {
-    // Show the Help page
     setShowHelp(true);
   };
 
-  const handleCloseHelp = () => {
-    // Close the Help page
+  const handleClose = () => {
+    setShowWorkouts(false);
+    setShowBodyStats(false);
     setShowHelp(false);
   };
 
@@ -39,16 +105,34 @@ export default function App() {
         <Text style={styles.buttonText}>Help</Text>
       </TouchableOpacity>
 
+      {showWorkouts && <WorkoutsOverlay onClose={handleClose} />}
+      {showBodyStats && <BodyStatsOverlay onClose={handleClose} />}
+
       {showHelp && (
-        <View style={styles.helpContainer}>
-          <Text style={styles.title}>Help</Text>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</Text>
-          <TouchableOpacity style={styles.closeButton} onPress={handleCloseHelp}>
+
+        <View style={styles.overlayContainer}>
+          <Text style={styles.title}>Welcome to Strongman+</Text>
+          <Text style={styles.subtitle}>Getting Started</Text>
+          <Text>Thank you for choosing Strongman+ to be your fitness companion! Whether you're a seasoned gym-goer or just starting your fitness journey. Below, we'll guide you through the key features and functionalities of the app.</Text>
+
+          <Text style={styles.sectionTitle}>Main Screen</Text>
+          <Text>Upon launching Strongman+, you'll find two main buttons. Workouts: Navigate here to create and track your workout routines. Body Stats: Track your body stats and input valuable information about your fitness journey.</Text>
+
+          <Text style={styles.sectionTitle}>Body Stats</Text>
+          <Text>When you select "Body Stats," you can log various weights, the current date, and other relevant information. Tap on cells to change their color, indicating improvement or areas that need attention.</Text>
+
+          <Text style={styles.sectionTitle}>Workouts</Text>
+          <Text>The "Workouts" menu lets you track different types of exercises. You can create custom buttons for your workout categories, such as Back/Arms, Legs, Chest/Shoulders, etc. Each button links to a new, customizable spreadsheet for tracking your progress. Again, tap on cells to change their color, indicating improvement or areas that need attention.</Text>
+
+          <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
             <Text style={styles.buttonText}>Close</Text>
           </TouchableOpacity>
         </View>
+
       )}
-    </View>
+
+
+    </View >
   );
 }
 
@@ -88,13 +172,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  helpContainer: {
+  overlayContainer: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -104,5 +188,45 @@ const styles = StyleSheet.create({
     backgroundColor: '#e74c3c',
     padding: 10,
     borderRadius: 10,
+  },
+  addContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: '#3498db',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 8,
+    marginRight: 10,
+    width: 150,
+  },
+  addButton: {
+    backgroundColor: '#2ecc71',
+    padding: 10,
+    borderRadius: 10,
+  },
+  subtitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  finalText: {
+    marginTop: 20,
+    marginBottom: 20,
+    fontStyle: 'italic',
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
   },
 });
